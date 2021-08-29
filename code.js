f6987695e8da034655f6b806e1eda0da218a711f
Aayushi Sharma <sharmaraayushi@gmail.com>
2021-08-29 20:14:17 +0530

    Initial commit

---
+++ code.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+    d=new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+
+    hr=htime*30 + mtime/2;
+    mr=6*mtime;
+    sr=6*stime;
+
+    hours.style.transform = `rotate(${hr}deg)`;
+    minutes.style.transform = `rotate(${mr}deg)`;
+    seconds.style.transform = `rotate(${sr}deg)`;
+}, 1000);
